7e317be0012462eed7569aa151c673241dbe361a
MAHIMA CHANDEL <75236972+Mahima507@users.noreply.github.com>
2023-07-08 19:46:46 +0530

    Create Gas_Station.java

---
+++ Array_Questions/Gas_Station.java
@@ -0,0 +1,20 @@
+class Solution {
+  public int canCompleteCircuit(int[] gas, int[] cost) {
+    final int gasses = Arrays.stream(gas).sum();
+    final int costs = Arrays.stream(cost).sum();
+    if (gasses - costs < 0)
+      return -1;
+
+    int ans = 0;
+    int sum = 0;
+    for (int i = 0; i < gas.length; ++i) {
+      sum += gas[i] - cost[i];
+      if (sum < 0) {
+        sum = 0;
+        ans = i + 1; // Start from next index
+      }
+    }
+
+    return ans;
+  }
+}
